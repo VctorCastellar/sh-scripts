90f5e3dc8a815a41619f5b19df418f5256861a56
VctorCastellar <sandrhenf@gmail.com>
2024-12-17 12:48:52 +0800

    Added 2 Shell script files

---
+++ exho.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello, I'm Vctor Castellar."
+echo "I'm working on a example script."
+echo "Scripts are really convenient."
+echo "And I like it."
+
+echo "Enter your age: "
+read age
+echo "You are: $age years old."
---
+++ installstuff.sh
@@ -0,0 +1,12 @@
+sudo apt-get install john -y
+sudo apt-get install nmap
+sudo apt-get install wget
+sudo apt-get install ssh
+sudo apt-get install nc
+sudo apt install -y nikto
+sudo apt install -y sqlmap
+sudo apt install -y metasploit-framework
+sudo apt install -y burpsuite
+sudo apt install -y wireshark
+sudo apt install -y hydra
+sudo apt-get install ncal
